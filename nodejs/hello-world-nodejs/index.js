846f02b391493592314a7770cadcea4b6fbe428a
buriashok <buri.ashokkumar@gmail.com>
2025-02-11 23:27:47 +0530

    written for 2 seconds delay message

--- nodejs/hello-world-nodejs/index.js
+++ nodejs/hello-world-nodejs/index.js
@@ -1,1 +1,7 @@
 console.log("Hello Node js");
+const array =[1,2,3,4];
+console.log(array, 'array');
+setTimeout(() => {
+console.log('this message is delayed by 2 seconds');
+}, 2000);
+console.log('this is the last line of the sync ');
