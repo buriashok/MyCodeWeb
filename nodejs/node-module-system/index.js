3210f4a1ea2cc4bdc9f925f6deff68cca559567e
buriashok <buri.ashokkumar@gmail.com>
2025-02-12 08:02:00 +0530

    done practical on wrapper demo

--- nodejs/node-module-system/index.js
+++ nodejs/node-module-system/index.js
@@ -1,2 +1,16 @@
 const firstModule = require('./math');
 console.log(firstModule.add(6,9));
+try {
+    console.log('try to divide by zero');
+    let result = firstModule.divide(40, 10);
+    console.log(result,'result');
+}catch(error) {
+    console.log('caught an error',error.message);
+}
+
+/*
+module wrapper 
+function(exports, require, module, __filename, __dirname){
+    // Your module code goes here
+    }
+*/
